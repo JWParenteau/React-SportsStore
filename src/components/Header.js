50645765d1b5f8f8ba89f00a46e2428defb83bc6
JWParenteau <justinwparenteau@hotmail.com>
2021-06-16 14:49:58 -0600

    Update workflow.

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Header() {
+    return (
+        <header>
+            <h1>Sports Store</h1>
+            <button>
+                <a href="">Sign Up</a>
+            </button>
+        </header>
+    );
+}
---
+++ src/components/ProductCategories.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function ProductCategories() {
+    return (
+    <section class="categories">
+        <ul>
+            <li>
+                <button> <a href="">Water Sports</a></button>
+            </li>
+            <li>
+                <button> <a href="">Soccer</a></button>
+            </li>
+            <li>
+                <button> <a href="">Basket Ball</a></button>
+            </li>
+            <li>
+                <button> <a href="">Hockey</a></button>
+            </li>
+            <li>
+                <button> <a href="">Board Games</a></button>
+            </li>
+        </ul>
+    </section>
+    );
+}
